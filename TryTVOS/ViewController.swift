59de7629e8c9ee93aa49fef09ebe5901ed55c8d2
Ben <b.cylin@gmail.com>
2016-02-19 20:21:32 +0800

    Set up tab bar controller in code

--- TryTVOS/ViewController.swift
+++ TryTVOS/ViewController.swift
@@ -30,12 +30,8 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    title = "Videos"
+    edgesForExtendedLayout = .None
   }
 
 }
